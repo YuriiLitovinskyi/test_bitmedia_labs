4ec7d83b400dc56ecdcaa1d0acac7681b8e432f1
YuriiLitovinskyi <y.litovinskyi@gmail.com>
2019-12-12 21:57:49 +0200

    styles

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -19,7 +19,9 @@ class Main extends React.Component {
 							<h2>AppCo</h2>
 							<h1><b>Brainstorming</b> for desired perfect Usability</h1>
 							<p>Our design projects are fresh and simple and will benefit your business greatly. Learn more about our work!</p>
-							<button className="stats">View Stats</button>
+							<Link to="/users">
+							    <button className="stats">View Stats</button>							 
+							</Link>							
 						</div>	
 						<div className="col-6">
 							<div className="mobile-wrap">
@@ -28,7 +30,38 @@ class Main extends React.Component {
 						</div>
 					</div>									
 				</div>
-			</header> 	    
+			</header> 
+			<div className="question">				
+				<h3>Why <b>small business owners</b> love AppCo?</h3>				
+			</div>	
+			<div className="about">
+				<p>Our design projects are fresh and simple and will benefit your business greatly. Learn more about our work!</p>
+			</div> 
+			<div className="Info">
+				<div className="row">				
+					<div className="col-3">
+						<div className="clean-design">
+							<img className="Group_13" src={Group_13} alt="Clean design img"></img>
+							<h3 className="CleanD">Clean Design</h3>
+							<p className="Sales">Increase sales by showing true dynamics of your website.</p>
+						</div>					
+					</div>
+					<div className="col-3">
+						<div className="clean-design">
+							<img className="Group_15" src={Group_15} alt="Secure Data img"></img>
+							<h3 className="SecureD">Secure Data</h3>
+							<p className="OnlineStore">Increase sales by showing true dynamics of your website.</p>
+						</div>										
+					</div>
+					<div className="col-3">
+						<div className="clean-design">
+							<img className="Group_14" src={Group_14} alt="Retina ready img"></img>
+							<h3 className="RetinaR">Retina Ready</h3>
+							<p className="SocialProof">Realize importance of social proof in customer’s purchase decision.</p>
+						</div>										
+					</div>									
+				</div>
+			</div>   
 	    </div>
   );
 
